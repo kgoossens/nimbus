94ec7a9fbb7c98013d97ba0b0e87631036a3bef5
Kristof Goossens <goossens@hpe.com>
2018-06-04 01:00:46 -0700

    Updated devops + made executable

--- create_setup.sh
+++ create_setup.sh
@@ -47,7 +47,7 @@ docker create -p 8000:8080 --name aos_main --hostname aosweb.aos.com -e "POSTGRE
 ############
 ## Devops ##
 ############
-docker create -p 8090:8080 -p 50000:50000 -p 9022:22 --name devops --hostname devops.aos.com --net demo-net admpresales/devops:1.1.3.4
+docker create -p 8090:8080 -p 8091:80 -p 50000:50000 -p 9022:22 --name devops --hostname devops.aos.com --net demo-net admpresales/devops:1.1.3.4
 
 ############
 ## Octane ##
