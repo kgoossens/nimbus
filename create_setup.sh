01e8880e42d26ac26d218242c6cd947f4554c0d7
Kristof <kristof@hpe.com>
2018-06-06 23:17:38 +0200

    updated Octane to 12.55.32.68_dis

--- create_setup.sh
+++ create_setup.sh
@@ -52,7 +52,7 @@ docker create -p 8090:8080 -p 50000:50000 -p 9022:22 --name devops --hostname de
 ############
 ## Octane ##
 ############
-docker create -p 1099:1099 -p 8085:8080 -p 9081:9081 -p 9082:9082 --name octane --hostname octane.aos.com --net demo-net -e OCTANE_HOST=nimbusserver.aos.com --shm-size=2g admpresales/octane:12.55.17.164_dis
+docker create -p 1099:1099 -p 8085:8080 -p 9081:9081 -p 9082:9082 --name octane --hostname octane.aos.com --net demo-net -e OCTANE_HOST=nimbusserver.aos.com --shm-size=2g admpresales/octane:12.55.32.68_dis
 
 
 ######################
